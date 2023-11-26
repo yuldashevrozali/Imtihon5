c82270159c0e6492389ebbd936a411c96a853fea
Yuldashevrozali <Yuldashevrozali08@gmail.com>
2023-11-26 12:08:22 +0500

    kill

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -28,6 +28,160 @@ fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_k
                 });
                 const main =  document.getElementById('main');
                 let lii = document.createElement('li');
+                let img = document.createElement('img');
+                img.src = `https://image.tmdb.org/t/p/w500${element.poster_path}`
+                lii.classList.add('lii');
+                lii.textContent = element.overview;
+                main.appendChild(img);
+                main.appendChild(lii);
+            })
+            
+
+            const p = document.createElement('p');
+            p.textContent = element.release_date;
+
+            Div.appendChild(h6)
+            Div.appendChild(li);
+            Div.appendChild(p);
+
+            Div.addEventListener('mouseover', () => {
+                Div.style.filter = 'blur(2px)';
+                Div.style.webkitFilter = 'blur(2px)';
+                Div.style.mozFilter = 'blur(2px)';
+                const icon = document.createElement('i');
+            });
+
+            Div.addEventListener('mouseout', () => {
+                Div.style.filter = 'none';
+                Div.style.webkitFilter = 'none';
+                Div.style.mozFilter = 'none';
+            });
+
+            fragment.appendChild(Div); 
+
+            Div && Div.addEventListener('click', function () {
+                console.log("jhhbv");
+            });
+        });
+
+        const main = document.getElementById('main');
+        main.appendChild(fragment); // DocumentFragmentdan main ga elementlarni qo'shish
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+    // fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=2")
+    // .then((data) => {
+    //     return data.json();
+    // })
+    // .then(dataJson => {
+    //     const fragment = document.createDocumentFragment();
+
+    //     const movies = dataJson.results;
+    //     console.log(movies);
+
+    //     movies.forEach(movie => {
+    //         const Div = document.createElement('div');
+    //         Div.classList.add('block');
+    //         Div.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${movie.poster_path})`;
+    //         Div.style.backgroundSize = 'cover';
+
+    //         const title = document.createElement('h5');
+    //         title.textContent = movie.title;
+
+    //         const moreButton = document.createElement('button');
+    //         moreButton.textContent = 'More';
+    //         moreButton.addEventListener('click', function () {
+    //             const main = document.getElementById('main');
+    //             main.innerHTML = ''; // Clear existing content before adding more details
+
+    //             const detailsDiv = document.createElement('div');
+    //             detailsDiv.classList.add('movie-details');
+
+    //             const overview = document.createElement('p');
+    //             overview.textContent = movie.overview;
+
+    //             const releaseDate = document.createElement('p');
+    //             releaseDate.textContent = `Release Date: ${movie.release_date}`;
+
+    //             const poster = document.createElement('img');
+    //             poster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    //             poster.alt = movie.title;
+
+    //             detailsDiv.appendChild(overview);
+    //             detailsDiv.appendChild(releaseDate);
+    //             detailsDiv.appendChild(poster);
+
+    //             main.appendChild(detailsDiv);
+    //         });
+
+    //         Div.appendChild(title);
+    //         Div.appendChild(moreButton);
+
+    //         Div.addEventListener('mouseover', () => {
+    //             Div.style.filter = 'blur(2px)';
+    //         });
+
+    //         Div.addEventListener('mouseout', () => {
+    //             Div.style.filter = 'none';
+    //         });
+
+    //         fragment.appendChild(Div);
+
+    //         Div && Div.addEventListener('click', function () {
+    //             console.log("Clicked on the movie");
+    //         });
+    //     });
+
+    //     const main = document.getElementById('main');
+    //     main.appendChild(fragment);
+    // })
+    // .catch((error) => {
+    //     console.log(error);
+    // });
+
+
+
+const all = document.getElementById('all');
+
+all && all.addEventListener('click', function () {
+    let name;
+
+    const FetchAll = async () => {
+        const fetch1 = awaitfetchData("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=10")
+        Newfetch()
+    }
+
+    fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=")
+    .then((data) => {
+        return data.json();
+    })
+    .then(dataJson => {
+        const fragment = document.createDocumentFragment();
+
+        const name = dataJson.results;
+        console.log(name);
+
+        name.forEach(element => {
+            const Div = document.createElement('div');
+            Div.classList.add('block');
+            Div.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${element.poster_path})`;
+            Div.style.backgroundSize = 'cover';
+
+            const li = document.createElement('li');
+            li.textContent = element.title;
+
+            const h6 = document.createElement('h6');
+            h6.textContent = 'More';
+            h6.addEventListener('click', function () {
+                const blocks = document.querySelectorAll('.block');
+                blocks.forEach(block => {
+                    block.style.display = 'none';
+                });
+                const main =  document.getElementById('main');
+                let lii = document.createElement('li');
+                let img = document.createElement('li');
                 lii.classList.add('lii');
                 lii.textContent = element.overview;
                 main.appendChild(lii);
@@ -67,56 +221,6 @@ fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_k
     .catch((error) => {
         console.log(error);
     });
-
-
-const all = document.getElementById('all');
-
-all && all.addEventListener('click', function () {
-    let name;
-
-    fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=")
-        .then((data) => {
-            return data.json();
-        })
-        .then(dataJson => {
-            name = dataJson.results;
-            const main = document.getElementById('main');
-            console.log(name);
-
-            name.forEach(element => {
-                const Div = document.createElement('div');
-                Div.classList.add('block');
-                Div.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${element.poster_path})`;
-                Div.style.backgroundSize = 'cover';
-
-                const li = document.createElement('li');
-                li.textContent = element.title;
-
-                const p = document.createElement('p');
-                p.textContent = element.release_date;
-
-                Div.appendChild(li);
-                Div.appendChild(p);
-
-                Div.addEventListener('mouseover', () => {
-                    Div.style.filter = 'blur(2px)';
-                    Div.style.webkitFilter = 'blur(2px)';
-                    Div.style.mozFilter = 'blur(2px)';
-                    const icon = document.createElement('i');
-                });
-
-                Div.addEventListener('mouseout', () => {
-                    Div.style.filter = 'none';
-                    Div.style.webkitFilter = 'none';
-                    Div.style.mozFilter = 'none';
-                });
-
-                main.appendChild(Div);
-            });
-        })
-        .catch((error) => {
-            console.log(error);
-        });
 })
 
 let name1;
